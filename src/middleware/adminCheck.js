a1a0af5a2727dd20a6f581133b867d1f1528868a
DeepayuNinama <138458194+DeepayuNinama@users.noreply.github.com>
2025-05-28 13:54:42 +0530

    Delete src directory

--- src/middleware/adminCheck.js
+++
@@ -1,9 +0,0 @@
-module.exports = function(req, res, next) {
-    if (req.isAuthenticated() && req.user.emailid==="adminmafia" || req.user.emailid==="adminmafia1") {
-      return next();
-    } else {
-      req.session.errorMessage = 'Access denied. Admins only.';
-      res.redirect('/dashboard');
-    }
-}
-  
--- src/middleware/isAuthenticated.js
+++
@@ -1,7 +0,0 @@
-module.exports = function (req, res, next) {
-    if (req.isAuthenticated()) {
-      return next();
-    }
-    res.redirect('/login'); // Redirect to login if not authenticated
-  };
-  
